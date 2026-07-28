76e091b1c54f17dbfa18366bd18135f31507a84e
dell <pagaredivyanka@gmail.com>
2026-07-28 18:50:02 +0530

    commit

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -13,11 +13,13 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //add first - O(1) constant time
     public void addFirst(int data) {
         //step1 - creat new node
         Node newNode = new Node(data);
+        size++;
         if(head == null) {
             head = tail = newNode;
             return;
@@ -32,6 +34,7 @@ public class LinkedList {
     //add last - O(1) constant time
     public void addLast(int data) {
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -61,6 +64,7 @@ public class LinkedList {
             addFirst(data);
             return;
         }
+        size++;
         Node newNode = new Node(data);
         Node temp = head;
         int i=0;
@@ -73,14 +77,201 @@ public class LinkedList {
         newNode.next = temp.next;
         temp.next = newNode;
     }
+
+    public int removeFirst() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size-2
+        Node prev = head;
+        for(int i=0; i<size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data; //tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int itrSearch(int key) { //O(n)
+        Node temp = head;
+        int i = 0;
+        while(temp != null) {
+            if(temp.data == key) { //key found
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        //key not found
+        return -1;
+    }
+
+    public int helper(Node head, int key) { //o(n)
+        if(head == null) {
+            return -1;
+        }
+        if(head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx==-1) {
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    // Reverse - classic imp 3 var 4 steps
+    public void reverse() { //O(n)
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void deleteNthfromEnd(int n) {
+        //calculate size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+        if(n == sz){
+            head = head.next; //removeFirst
+            return;
+        }
+
+        //sz-n
+        int i = 1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    //Check if LL is a Palindrome - vimp it teaches - slow(+1) fast(+2) concept & reverse half LL
+    // using AL , array, string - tc & sc O(n)
+    
+    //slow-fast approach
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        return slow; //slow is my midNode
+    }
+
+    public boolean checkPalindrom() {
+        if(head == null || head.next == null) {
+            return true;
+        }
+        //step1 - find mid
+        Node midNode = findMid(head);
+
+        //step2 - reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while(curr!=null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        Node right = prev; //right half head
+        Node left = head;
+
+        //step3 - check left half & right half
+        while(right != null) {
+            if(left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+
     public static void main(String args[]){
         LinkedList ll = new LinkedList(); //obj
-        ll.addFirst(2);
-        ll.addFirst(1);
-        ll.addLast(3);
-        ll.addLast(4);
-        ll.add(2, 9);
 
-        ll.print();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(1);
+        // ll.addLast(1);
+        ll.print(); //1->2->2->1->null
+        System.out.println(ll.checkPalindrom()); //true
+
+        // ll.addFirst(2);
+        // ll.addFirst(1);
+        // ll.addLast(4);
+        // ll.addLast(5);
+        // ll.add(2, 3);
+
+        // ll.print(); //1->2->3->4->5->null
+        // ll.deleteNthfromEnd(3);
+        // ll.print();
+        //--------------------------------------------------------
+        // System.out.println(ll.itrSearch(3)); //2
+        // System.out.println(ll.itrSearch(10)); //-1
+
+        // System.out.println(ll.recSearch(3)); //2
+        // System.out.println(ll.recSearch(10)); //-1
+
+        // ll.reverse(); //5->4->3->2->1->null
+        // ll.print();
+        //----------------------------------------------------------------
+        // System.out.println(ll.size); //5 O(1) constant time
+        // ll.removeFirst(); //2->3->4->5->null
+        // ll.print();
+
+        // ll.removeLast();//2->3->4->null
+        // ll.print();
+
+        // System.out.println(ll.size); //3
+       
     }
 }
