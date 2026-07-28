fc2acb1fc887c069d4ea0c728e5490bfae11f810
dell <pagaredivyanka@gmail.com>
2026-07-28 16:52:20 +0530

    commit

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,68 @@
+package linkedlist;
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        //constructor
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    //add first - O(1) constant time
+    public void addFirst(int data) {
+        //step1 - creat new node
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        //step2 - newNode next = head
+        newNode.next = head; //link
+
+        //step3 - head = newNode
+        head = newNode;
+    }
+        
+    //add last - O(1) constant time
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() { //O(n) linear time
+        // if(head == null) {
+        //     System.out.println("LL is empty");
+        //     return;
+        // }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]){
+        LinkedList ll = new LinkedList(); //obj
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+    }
+}
