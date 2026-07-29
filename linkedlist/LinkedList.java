299d143120cef31c56e369b885acc764098a42f8
dell <pagaredivyanka@gmail.com>
2026-07-29 16:55:18 +0530

    detect cycle

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -234,16 +234,37 @@ public class LinkedList {
         return true;
     }
 
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
 
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next; 
+            if(slow == fast) {
+                return true; //cycle exists
+            }
+        }
+        return false; //cycle does not exists
+    }
     public static void main(String args[]){
-        LinkedList ll = new LinkedList(); //obj
 
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(1);
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        //1->2->3->1
+        System.out.println(isCycle()); //true
+
+        //****************************************************************************
+        // LinkedList ll = new LinkedList(); //obj
+
         // ll.addLast(1);
-        ll.print(); //1->2->2->1->null
-        System.out.println(ll.checkPalindrom()); //true
+        // ll.addLast(2);
+        // ll.addLast(1);
+        // // ll.addLast(1);
+        // ll.print(); //1->2->2->1->null
+        // System.out.println(ll.checkPalindrom()); //true
 
         // ll.addFirst(2);
         // ll.addFirst(1);
