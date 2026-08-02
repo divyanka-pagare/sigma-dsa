b5ee76aebd63def621d7a41745985512397d40b6
dell <pagaredivyanka@gmail.com>
2026-08-02 16:51:02 +0530

    zigzag

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -332,17 +332,53 @@ public class LinkedList {
         return merge(newLeft, newRight);
     }
 
+    public void zigZag() {
+        //find mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //reverse 2nd half
+        Node curr = slow.next;
+        slow.next = null;
+        Node prev = null;
+        Node next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        //alt merge - zig-zag merge
+        while(left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
     public static void main(String args[]){
 
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addFirst(3);
-        ll.addFirst(4);
-        ll.addFirst(5);
-        //5->4->3->2->1->null
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        //1->2->3->4->5->null
         ll.print();
-        ll.head = ll.mergeSort(ll.head);
+        ll.zigZag();
         ll.print();
 
 
