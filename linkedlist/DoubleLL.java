16daf4acc627f1d7ff985eca9f1711b8892176e8
dell <pagaredivyanka@gmail.com>
2026-08-02 18:48:14 +0530

    reverse

--- linkedlist/DoubleLL.java
+++ linkedlist/DoubleLL.java
@@ -101,12 +101,30 @@ public class DoubleLL {
         return val;
     }
 
+    //reverse
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;   
+        }
+        head = prev;
+    }
+
     public static void main(String args[]) {
         DoubleLL dll = new DoubleLL();
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
         
+        dll.print(); //1<->2<->3<->null
+        dll.reverse(); //3<->2<->1<->null
         dll.print();
         System.out.println(dll.size);
 
