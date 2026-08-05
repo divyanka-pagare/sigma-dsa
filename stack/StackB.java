991038aa7143e5975c78793618c64085651580b6
dell <pagaredivyanka@gmail.com>
2026-08-05 19:09:46 +0530

    stack using linkedlist

--- stack/StackB.java
+++ stack/StackB.java
@@ -2,35 +2,83 @@ package stack;
 import java.util.*;
 
 public class StackB {
-    static class Stack {
-        static ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isEmpty() {
-            return list.size() == 0;
-        }
 
-        //push 
-        public static void push(int data) {
-            list.add(data);
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
-
-        //pop
-        public static int pop() {
-            if(isEmpty()) {
-                return -1;
+        static class Stack {
+            static Node head = null;
+            public static boolean isEmpty() {
+                return head == null;
             }
-            int top = list.get(list.size() - 1);
-            list.remove(list.size()-1);
-            return top;
-        }
 
-        //peek
-        public static int peek() {
-            if(isEmpty()) {
-                return -1;
+            //push
+            public static void push(int data) {
+                Node newNode = new Node(data);
+                if(isEmpty()) {
+                    head = newNode;
+                    return;
+                }
+                newNode.next = head;
+                head = newNode;
             }
-            return list.get(list.size() - 1);
+
+            //pop
+            public static int pop() {
+                if(isEmpty()){
+                    return -1;
+                }
+                int top = head.data;
+                head = head.next;
+                return top;
+            }
+
+            //peek
+            public static int peek() {
+                if(isEmpty()) {
+                    return -1;
+                }
+                return head.data;
+            }
         }
     }
+
+
+    // static class Stack {
+    //     static ArrayList<Integer> list = new ArrayList<>();
+    //     public static boolean isEmpty() {
+    //         return list.size() == 0;
+    //     }
+
+    //     //push 
+    //     public static void push(int data) {
+    //         list.add(data);
+    //     }
+
+    //     //pop
+    //     public static int pop() {
+    //         if(isEmpty()) {
+    //             return -1;
+    //         }
+    //         int top = list.get(list.size() - 1);
+    //         list.remove(list.size()-1);
+    //         return top;
+    //     }
+
+    //     //peek
+    //     public static int peek() {
+    //         if(isEmpty()) {
+    //             return -1;
+    //         }
+    //         return list.get(list.size() - 1);
+    //     }
+    // }
+    
+    
     public static void main(String args[]) {
         Stack s = new Stack();
         s.push(1);
