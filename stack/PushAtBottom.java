11a3bfcf79df2051b7f4d067adbd023bea782c46
dell <pagaredivyanka@gmail.com>
2026-08-09 20:59:10 +0530

    reverse stack

--- stack/PushAtBottom.java
+++ stack/PushAtBottom.java
@@ -10,39 +10,59 @@ public class PushAtBottom {
             s.push(data);
             return;
         }
-
         int top = s.pop();
         pushAtBottom(s, data);
         s.push(top);
     }
 
-    public static String reverseString(String str) {
-        Stack<Character> s = new Stack<>();
+    // public static String reverseString(String str) {
+    //     Stack<Character> s = new Stack<>();
 
-        int idx = 0;
+    //     int idx = 0;
 
-        while (idx < str.length()) {
-            s.push(str.charAt(idx));
-            idx++;
+    //     while (idx < str.length()) {
+    //         s.push(str.charAt(idx));
+    //         idx++;
+    //     }
+
+    //     StringBuilder result = new StringBuilder("");
+
+    //     while (!s.isEmpty()) {
+    //         char curr = s.pop();
+    //         result.append(curr);
+    //     }
+    //     return result.toString();
+    // }
+
+    public static void reverseStack(Stack<Integer> s) {
+        if(s.isEmpty()) {
+            return;
         }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
 
-        StringBuilder result = new StringBuilder("");
-
-        while (!s.isEmpty()) {
-            char curr = s.pop();
-            result.append(curr);
+    public static void printStack(Stack<Integer> s) {
+        while(!s.isEmpty()) {
+            System.out.println(s.pop());
         }
-
-        return result.toString();
     }
 
 
-
     public static void main(String args[]) {
-        String str = "ritesh";
 
-        String result = reverseString(str);
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
 
-        System.out.println(result);
+        printStack(s);
+        reverseStack(s);
+        printStack(s);
+
+        // String str = "ritesh";
+        // String result = reverseString(str);
+        // System.out.println(result);
     }
 }
