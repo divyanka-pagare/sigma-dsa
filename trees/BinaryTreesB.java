75cc58a94a946cbc4677db160505c5f40a6b520e
dell <pagaredivyanka@gmail.com>
2026-09-04 00:08:47 +0530

    level order

--- trees/BinaryTreesB.java
+++ trees/BinaryTreesB.java
@@ -1,5 +1,6 @@
 package trees;
 
+import java.util.*;
 public class BinaryTreesB {
     static class Node {
         int data;
@@ -53,6 +54,36 @@ public class BinaryTreesB {
             postorder(root.right);
             System.out.print(root.data + " ");
         }
+
+        //Level order traversal
+        public static void levelorder(Node root) {
+            if(root == null) {
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()) {
+                Node currNode = q.remove();
+                if(currNode == null) {
+                    System.out.println();
+                    if(q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if(currNode.left != null) {
+                        q.add(currNode.left);
+                    } 
+                    if(currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String args[]) {
@@ -68,6 +99,7 @@ public class BinaryTreesB {
         tree.inorder(root);
         System.out.println();
         tree.postorder(root);   
-
+        System.out.println();
+        tree.levelorder(root);
     }
 }
